b97ab3dc4d2382e4a7ba23693673baad78557014
Kevin William <kevin_casapc@hotmail.com>
2021-11-05 22:35:15 -0300

    Exercicio Loja de games

---
+++ lojagames/src/main/java/br/org/generation/lojagames/model/Categoria.java
@@ -0,0 +1,54 @@
+package br.org.generation.lojagames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull (message = "Categoria")
+	@Size(min = 2)
+	private String titulo;
+	
+	@NotNull (message = "Descricao")
+	@Size (min = 2)
+	private String descricao;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	
+	
+}
