bc27c5141cbfb272976bb3e0e728915eb2440cc0
Kevin William <kevin_casapc@hotmail.com>
2021-11-08 21:21:50 -0300

    Exercicio Farmacia

---
+++ farmacia/src/main/java/br/org/generation/farmacia/model/Categoria.java
@@ -0,0 +1,47 @@
+package br.org.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table (name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank (message = "A categoria não pode ser vazia")
+	private String categoria;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	private String descricao;
+
+}
